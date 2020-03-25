65a59511e8b6dfa3acf1419617673aad8b3cd622
Kevin Mann <kevinjamesmann@gmail.com>
2020-03-25 09:15:01 +0900

    rebuild

--- src/utils/loadAndProcessData.js
+++ src/utils/loadAndProcessData.js
@@ -4,7 +4,7 @@ export const loadAndProcessData = () =>
 
         Promise.all([
             d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json'),
-            d3.json('/lda_visualizations/countryTopics.json')
+            d3.json('/countryTopics.json')
             // d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv')
         ])
         .then(([topoData, countryTopics]) => {
